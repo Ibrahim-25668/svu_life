66a7dc5296e6c967574a87877526fe49d6ac2f97
Ibrahim Ghanem <ibrahimghanem052@gmail.com>
2026-05-06 19:40:41 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// إظهار الزر عند النزول
+window.onscroll = function () {
+    let btn = document.getElementById("topBtn");
+
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// عند الضغط
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
